b22f6348dc12af76c5000cfa2b72827f52ba6b1d
Philipp Stöger <if13b043@technikum-wien.at>
2014-11-25 16:02:31 +0100

    Marvin Framework added. Erste Testimplementierungen - print Picture

--- src/server/CoreServlet.java
+++ src/server/CoreServlet.java
@@ -1,8 +1,12 @@
 package server;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,14 +22,62 @@ public class CoreServlet extends HttpServlet
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
-		PrintWriter out = response.getWriter();
-		out.println("Hello World");
+		
+//		PrintWriter out = response.getWriter();
+//		Vectorizer myVectorizer = new Vectorizer();
+//		out.println(myVectorizer.getmString());
+		PrintPicture(response, request.getParameter("Pfad"));
+		
 	}
+	
 
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
 		PrintWriter out = response.getWriter();
 		out.println("Hello World");
+	}
+	
+	public void PrintPicture(HttpServletResponse resp, String path) throws IOException 
+	{
+		ServletContext cntx= getServletContext();
+		// Get the absolute path of the image
+		String filename = cntx.getRealPath("Images/button.png");
+		if((path != "") || (path != null))
+			filename = cntx.getRealPath("Images/" + path);
+		
+		File file = new File(filename);
+		if(!file.exists())
+		{
+			PrintWriter out = resp.getWriter();
+			Vectorizer myVectorizer = new Vectorizer();
+			out.println("Datei nicht gefunden");
+			out.println("realPath: " + filename);
+			return;
+		}
+		
+		// retrieve mimeType dynamically
+		String mime = cntx.getMimeType(filename);
+		if (mime == null) 
+		{
+			resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			return;
+		}
+	
+		resp.setContentType(mime);
+		resp.setContentLength((int)file.length());
+	
+		FileInputStream in = new FileInputStream(file);
+		OutputStream out = resp.getOutputStream();
+	
+		// Copy the contents of the file to the output stream
+		byte[] buf = new byte[1024];
+		int count = 0;
+		
+		while ((count = in.read(buf)) >= 0) 
+			out.write(buf, 0, count);
+		
+		out.close();
+		in.close();
 	}
 }
--- src/server/Vectorizer.java
+++ src/server/Vectorizer.java
@@ -2,8 +2,15 @@ package server;
 
 public class Vectorizer
 {
+	private String mString;
 	public Vectorizer()
 	{
+		mString = "lolwat";
 		
+	}
+	
+	public String getmString()
+	{
+		return mString;
 	}
 }
