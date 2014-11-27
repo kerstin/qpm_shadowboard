a0e5a6b13b0e6f88ed47c556c66c71b5f43a0239
Philipp Stöger <if13b043@technikum-wien.at>
2014-11-27 16:12:11 +0100

    Marvin Framework implementiert. Alle Bilder werden mit EdgeDetector und Inverter behandelt und als <filename>.edited.<fileending> gespeichert.

--- src/server/CoreServlet.java
+++ src/server/CoreServlet.java
@@ -13,11 +13,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import marvin.MarvinDefinitions;
+import marvin.image.MarvinImage;
+import marvin.io.MarvinImageIO;
+import marvin.plugin.MarvinImagePlugin;
+import marvin.util.MarvinPluginLoader;
+
 
 @WebServlet("/app")
 public class CoreServlet extends HttpServlet 
 {
 	private static final long serialVersionUID = 1L;
+	private MarvinImage		image, backupImage;
+	private MarvinImagePlugin     imagePlugin;
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
@@ -42,7 +50,7 @@ public class CoreServlet extends HttpServlet
 	{
 		ServletContext cntx= getServletContext();
 		// Get the absolute path of the image
-		String filename = cntx.getRealPath("Images/button.png");
+		String filename = cntx.getRealPath("Images/button_.png");
 		if((path != "") || (path != null))
 			filename = cntx.getRealPath("Images/" + path);
 		
@@ -50,24 +58,43 @@ public class CoreServlet extends HttpServlet
 		if(!file.exists())
 		{
 			PrintWriter out = resp.getWriter();
-			Vectorizer myVectorizer = new Vectorizer();
+			//Vectorizer myVectorizer = new Vectorizer();
 			out.println("Datei nicht gefunden");
 			out.println("realPath: " + filename);
 			return;
 		}
 		
+		MarvinDefinitions.setImagePluginPath("D:/Dropbox/FH/ITP3/Shadowboard/marvin/plugins/image/");
+		image = MarvinImageIO.loadImage(filename);
+		backupImage = image.clone();        
+		imagePlugin = MarvinPluginLoader.loadImagePlugin("org.marvinproject.image.edge.edgeDetector.jar");
+		imagePlugin.process(image, image);
+		imagePlugin = MarvinPluginLoader.loadImagePlugin("org.marvinproject.image.color.invert.jar");
+		imagePlugin.process(image, image);
+        image.update();
+        
+        String newFilename = filename;
+        int Index = newFilename.lastIndexOf('.');
+        String nameOfFile = newFilename.substring(0, Index);
+        String endingOfFile = newFilename.substring(Index, newFilename.length());
+        newFilename = nameOfFile + ".edited" + endingOfFile;
+        
+        MarvinImageIO.saveImage(image, newFilename);
+        System.out.println("Saved file as: " + newFilename);
+		
 		// retrieve mimeType dynamically
-		String mime = cntx.getMimeType(filename);
+		String mime = cntx.getMimeType(newFilename);
 		if (mime == null) 
 		{
 			resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 			return;
 		}
 	
+		File NewFile = new File(newFilename);
 		resp.setContentType(mime);
-		resp.setContentLength((int)file.length());
+		resp.setContentLength((int)NewFile.length());
 	
-		FileInputStream in = new FileInputStream(file);
+		FileInputStream in = new FileInputStream(NewFile);
 		OutputStream out = resp.getOutputStream();
 	
 		// Copy the contents of the file to the output stream
